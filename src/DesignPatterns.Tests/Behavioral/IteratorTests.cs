ef9794d4faa7d73aa69c5461f23251ba2a673cbc
Allen Chen <Allen@INSELLERATE.LOCAL>
2020-07-19 23:34:54 -0700

    check in iterator tests

---
+++ src/DesignPatterns.Tests/Behavioral/IteratorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+
+namespace DesignPatterns.Tests.Behavioral
+{
+    public class IteratorTests
+    {
+        /* 
+            Pattern     :   Iterator
+            References  :
+                            https://refactoring.guru/design-patterns/iterator
+                            https://sourcemaking.com/design_patterns/iterator
+            Real World  :   
+                            different ways of traversing a collection
+                            maze
+                            
+        */
+
+        [Fact]
+        public void client()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+    }
+}
