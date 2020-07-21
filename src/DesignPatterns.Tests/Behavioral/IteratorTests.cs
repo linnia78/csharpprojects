7354fd61dd0b7470d890c8a5a28b79e05eb4ae17
Allen Chen <allen16@allens-mbp.lan>
2020-07-20 22:56:47 -0700

    Add iterator tests

--- src/DesignPatterns.Tests/Behavioral/IteratorTests.cs
+++ src/DesignPatterns.Tests/Behavioral/IteratorTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace DesignPatterns.Tests.Behavioral
@@ -20,11 +22,73 @@ namespace DesignPatterns.Tests.Behavioral
         public void client()
         {
             // Arrange
+            var collection = new Collection(new List<int> { 3, 6, 1, 7, 2, 9 });
+            var index = 0;
 
             // Act
+            var first = 0;
+            var last = 0;
+            var third = 0;
+            foreach(var item in collection)
+            {
+                if (index == 0)
+                {
+                    first = (int)item;
+                }
+                else if (index == 2)
+                {
+                    third = (int)item;
+                }
+                else if (index == collection.Count - 1)
+                {
+                    last = (int)item;
+                }
+                index++;
+            }
 
             // Assert
+            Assert.Equal(1, first);
+            Assert.Equal(3, third);
+            Assert.Equal(9, last);
         }
 
+        public class Collection : IEnumerable
+        {
+            private List<int> _list;
+            public Collection(List<int> list)
+            {
+                _list = list;
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                return new SortedIterator(this._list);
+            }
+
+            public int Count => _list.Count;
+        }
+
+        public class SortedIterator : IEnumerator
+        {
+            private int index = -1;
+            private readonly List<int> _list;
+
+            public object Current => _list[index];
+
+            public SortedIterator(List<int> list)
+            {
+                _list = list.OrderBy(x => x).ToList();
+            }
+
+            public bool MoveNext()
+            {
+                return index++ < _list.Count - 1;
+            }
+
+            public void Reset()
+            {
+                index = 0;
+            }
+        }
     }
 }
