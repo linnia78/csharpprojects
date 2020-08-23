cdadae6d4628bce9d73e314faf31c2a886320899
Allen Chen <allen16@allens-mbp.lan>
2020-08-22 23:31:39 -0700

    Add palindrome and merge point tests

---
+++ src/Algorithms.Tests/Cracking/LinkedLists/_6_Palindrome.cs
@@ -0,0 +1,148 @@
+﻿using Algorithms.Tests.DataStructures.Node;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Algorithms.Tests.Cracking.LinkedLists
+{
+    public class _6_Palindrome
+    {
+        /// <summary>
+        /// Check if linked list is palindrome
+        /// </summary>
+        public _6_Palindrome()
+        {
+            
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1 }, true)]
+        [InlineData(new int[] { 1, 2, 1 }, true)]
+        [InlineData(new int[] { 1, 2, 2, 1 }, true)]
+        [InlineData(new int[] { 1, 2, 3 }, false)]
+        [InlineData(new int[] { 1, 2, 3, 1 }, false)]
+        public void should_check_if_palindrome_iteratively(int[] array, bool expected)
+        {
+            // Algorithm
+            // create fast pointer which moves at 2 x
+            // create slow pointer, while traversing add value to stack
+            // when end is reached the slow is at mid
+            // continue iteration and compare with stack
+
+            // Arrange
+            var node = Node<int>.Create(array);
+
+            // Act
+            var isPalindrome = IsPalindromeIteratively(node);
+
+            // Assert
+            Assert.Equal(expected, isPalindrome);
+        }
+
+        private bool IsPalindromeIteratively(Node<int> node)
+        {
+            // traverse till mide and add to stack
+            var slow = node;
+            var fast = node;
+            var stack = new Stack<int>();
+            while (slow != null && fast != null && fast.Next != null)
+            {
+                stack.Push(slow.Value);
+                slow = slow.Next;
+                fast = fast.Next.Next;
+            }
+
+            // if fast == null then even / if fast != null then odd
+            // if even then compare current with stack
+            // if odd then compare starting from next with stack, "skip one"
+            if (fast != null)
+            {
+                slow = slow.Next;
+            }
+
+            // iterate till end of loop, return false if mismatch found
+            while(slow != null)
+            {
+                if (stack.Pop() != slow.Value)
+                {
+                    return false;
+                }
+                slow = slow.Next;
+            }
+            return true;
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1 }, true)]
+        [InlineData(new int[] { 1, 2, 1 }, true)]
+        [InlineData(new int[] { 1, 2, 2, 1 }, true)]
+        [InlineData(new int[] { 1, 2, 3 }, false)]
+        [InlineData(new int[] { 1, 2, 3, 1 }, false)]
+        public void should_check_if_palindrome_recursively(int[] array, bool expected)
+        {
+            // Algorithm - get length of linked list
+            // Iterate till mid of list by subtracting length by 2
+            // if length is 0 then even -> return current node as we would be on the node that needs to be used to compare
+            // if length is 1 then odd -> return next node
+            // during comparision, compare the recursion returned node and pass back the next node for call stack to compare
+            // Arrange
+            var node = Node<int>.Create(array);
+
+            // Act
+            var isPalindrome = IsPalindromeRecursively(node);
+
+            // Assert
+            Assert.Equal(expected, isPalindrome);
+        }
+
+        private bool IsPalindromeRecursively(Node<int> node)
+        {
+            var length = GetSize(node);
+            return IsPalindromeIterativelyHelper(node, length).IsMatch;
+        }
+
+        private Result IsPalindromeIterativelyHelper(Node<int> current, int length)
+        {
+            if (length == 0)
+            {
+                // if 0 then even, pass back current
+                return new Result(current, true);
+            } 
+            else if (length == 1)
+            {
+                // if 1 then odd, pass back next
+                return new Result(current.Next, true);
+            }
+
+            var result = IsPalindromeIterativelyHelper(current.Next, length - 2);
+            if (result.IsMatch == false)
+            {
+                return new Result(null, false);
+            }
+            return new Result(result.Node.Next, result.Node.Value == current.Value);
+        }
+
+        private int GetSize(Node<int> node)
+        {
+            var count = 0;
+            while(node != null)
+            {
+                count++;
+                node = node.Next;
+            }
+            return count;
+        }
+
+        private class Result
+        {
+            public Node<int> Node { get; }
+            public bool IsMatch { get; }
+            public Result(Node<int> node, bool isMatch)
+            {
+                Node = node;
+                IsMatch = isMatch;
+            }
+        }
+    }
+}
---
+++ src/Algorithms.Tests/Cracking/LinkedLists/_7_MergePoint.cs
@@ -0,0 +1,117 @@
+﻿using Algorithms.Tests.DataStructures.Node;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Algorithms.Tests.Cracking.LinkedLists
+{
+    public class _7_MergePoint
+    {
+        /// <summary>
+        /// Find merge point between two singly linked list
+        /// Guaranteed that there's a merge point
+        /// </summary>
+        public _7_MergePoint()
+        {
+            
+        }
+
+        [Theory]
+        //   1 - null
+        //  /
+        // 1 - null
+        [InlineData(new int[] { 1 }, new int[] { 1 }, 1)]
+        //       1 - 2 - 3 - 4 - 5 - null
+        //          /
+        // 1 - 2 - 3
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3 }, 2)]
+        //               1 - 2 - 3 - null
+        //                  /
+        // 1 - 2 - 3 - 4 - 5
+        [InlineData(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3, 4, 5 }, 2)]
+        // 1 - 2 - 3 - 4 - 5 - null
+        //        /
+        //   1 - 2
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2 }, 3)]
+        public void should_check_if_palindrome_iteratively(int[] a, int[] b, int mergeAtIndex)
+        {
+            // Algorithm
+            // get size of both list
+            // traverse longer list to node by x where x = difference in length
+            // traverse both node until they match
+            var test = a == b;
+
+            // Arrange
+            var nodeA = Node<int>.Create(a);
+            var nodeB = Node<int>.Create(b);
+            var expected = MergeTestHelper(nodeA, nodeB, mergeAtIndex);
+
+            // Act
+            var mergePoint = FindMergeNode(nodeA, nodeB);
+
+            // Assert
+            Assert.Equal(expected, mergePoint);
+        }
+
+        private Node<int> MergeTestHelper(Node<int> target, Node<int> source, int mergeAtIndex)
+        {
+            // merge source into target at target's "mergeAtIndex"
+            var pointer = target;
+            while(mergeAtIndex-- > 1 && pointer != null)
+            {
+                pointer = pointer.Next;
+            }
+
+            var endOfSource = source;
+            while(endOfSource != null && endOfSource.Next != null)
+            {
+                endOfSource = endOfSource.Next;
+            }
+
+            endOfSource.Next = pointer;
+            return pointer;
+        }
+
+        private Node<int> FindMergeNode(Node<int> a, Node<int> b)
+        {
+            // Get size differenc
+            var aSize = GetSize(a);
+            var bSize = GetSize(b);
+            var sizeDifference = Math.Abs(aSize - bSize);
+
+            // Move pointer forward for longer list so starting position is the same
+            var longNode = aSize > bSize ? a : b;
+            var shortNode = aSize <= bSize ? a : b;
+            while(sizeDifference-- > 0)
+            {
+                longNode = longNode.Next;
+            }
+
+            // Find merge node
+            while(longNode != null && shortNode != null)
+            {
+                if (longNode == shortNode)
+                {
+                    return longNode;
+                }
+
+                longNode = longNode.Next;
+                shortNode = shortNode.Next;
+            }
+            return null; // technically will never be reached because of constraint that a merge exists
+        }
+
+        private int GetSize(Node<int> node)
+        {
+            var count = 0;
+            while(node != null)
+            {
+                count++;
+                node = node.Next;
+            }
+            return count;
+        }
+
+    }
+}
